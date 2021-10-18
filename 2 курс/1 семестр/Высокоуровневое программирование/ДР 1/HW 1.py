31dcae6f323b3afbdc1c089d1cc9e2afd45cdfb5
Multiname <minefend@gmail.com>
2021-10-18 19:19:08 +0300

    ВП ДР 1 - готовый код

--- 2 курс/1 семестр/Высокоуровневое программирование/ДР 1/HW 1.py
+++ 2 курс/1 семестр/Высокоуровневое программирование/ДР 1/HW 1.py
@@ -2,14 +2,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QColor, QBrush
 import sys
 from Interface import *
+import matplotlib.pyplot as plt
+import math
 
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
+        self.activatedLight = 0
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
                   
-        self.ui.pushButton.clicked.connect(self.buttonpress)
+        self.ui.GraphButton.clicked.connect(self.PressGraph)
+
+        self.ui.RedButton.toggled.connect(self.PressRed)
+        self.ui.YellowButton.toggled.connect(self.PressYellow)
+        self.ui.GreenButton.toggled.connect(self.PressGreen)
+
+        self.ui.HelpButton.clicked.connect(self.PressHelp)
 
     def paintEvent(self, e):
         qp = QPainter()
@@ -22,17 +31,82 @@ class MyWin(QtWidgets.QMainWindow):
         col.setNamedColor('#d4d4d4')
         qp.setPen(col)
 
-        qp.setBrush(QColor(200, 0, 0))
-        qp.drawRect(150, 30, 60, 60)
+        if self.activatedLight == 0:
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 60, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 120, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 180, 60, 60)
+            
+        elif self.activatedLight == 1:
+            qp.setBrush(QColor(230, 0, 0))
+            qp.drawRect(160, 60, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 120, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 180, 60, 60)
 
-        qp.setBrush(QColor(255, 80, 0, 160))
-        qp.drawRect(150, 90, 60, 60)
+        elif self.activatedLight == 2:
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 60, 60, 60)
+            
+            qp.setBrush(QColor(247, 231, 0))
+            qp.drawRect(160, 120, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 180, 60, 60)
 
-        qp.setBrush(QColor(25, 0, 90, 200))
-        qp.drawRect(150, 150, 60, 60)
+        elif self.activatedLight == 3:
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 60, 60, 60)
+            
+            qp.setBrush(QColor(255, 255, 255))
+            qp.drawRect(160, 120, 60, 60)
+            
+            qp.setBrush(QColor(0, 171, 17))
+            qp.drawRect(160, 180, 60, 60)
                 
-    def buttonpress(self):
-        print("OK")
+    def PressGraph(self):
+        x1 = [n/10 for n in range(-40, 0)]
+        x2 = [n/10 for n in range(1, 41)]
+        y1 = [abs(math.cos(2*n)*math.sin(3*n))/math.sin(n) for n in x1]
+        y2 = [abs(math.cos(2*n)*math.sin(3*n))/math.sin(n) for n in x2]
+        ax = [0, 0]
+        ay = [-4, 4]
+    
+        plt.text(0.5, 3.5, "y = |cos(2x)sin(2x)|/sin(x)")
+        plt.plot(x1, y1, x2, y2, ax, ay, '--')
+        plt.show()
+
+    def PressRed(self):
+        button = self.sender()
+        if button.isChecked():
+            self.activatedLight = 1
+            self.update()
+
+    def PressYellow(self):
+        button = self.sender()
+        if button.isChecked():
+            self.activatedLight = 2
+            self.update()
+
+    def PressGreen(self):
+        button = self.sender()
+        if button.isChecked():
+            self.activatedLight = 3
+            self.update()
+
+    def PressHelp(self):
+        message = QtWidgets.QMessageBox()
+        message.setIcon(QtWidgets.QMessageBox.Information)
+        message.setText("Обращение за справкой")
+        message.setWindowTitle("Справка")
+        message.exec_()
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
