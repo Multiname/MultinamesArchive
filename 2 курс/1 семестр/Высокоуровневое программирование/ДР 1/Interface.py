6e37becd08f7907f00e56f8956909f80e86c2009
Multiname <minefend@gmail.com>
2021-10-21 16:50:29 +0300

    ВП ДР 1 - отчет

--- 2 курс/1 семестр/Высокоуровневое программирование/ДР 1/Interface.py
+++ 2 курс/1 семестр/Высокоуровневое программирование/ДР 1/Interface.py
@@ -1,10 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(264, 313)
+        MainWindow.setFixedSize(264, 313)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.GraphButton = QtWidgets.QPushButton(self.centralwidget)
