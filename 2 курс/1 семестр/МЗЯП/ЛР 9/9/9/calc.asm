edb97513b34e08f4004803e09675314779b991fc
Multiname <minefend@gmail.com>
2021-12-01 14:12:35 +0300

    МЗЯП ЛР 9 - код

--- 2 курс/1 семестр/МЗЯП/ЛР 9/9/9/calc.asm
+++ 2 курс/1 семестр/МЗЯП/ЛР 9/9/9/calc.asm
@@ -1,19 +1,24 @@
 .586
 .MODEL FLAT, C
 .DATA
-one dd 1.0
 two dd 2.0
 six dd 6.0
 
+lowerBound dd 1.1
+upperBound dd 5.1
+nine       dd 9.0
+fsttmp     dw 0
+
 .CODE
 calc proc C x:qword
+finit
 fld x               ; ST(0) = x
 fld x               ; ST(0) = x, ST(1) = x
 fmul ST(0), ST(1)   ; ST(0) = x^2, ST(1) = x
 fmul ST(0), ST(1)   ; ST(0) = x^3, ST(1) = x
 fmul ST(0), ST(1)   ; ST(0) = x^4, ST(1) = x
 
-fld one             ; ST(0) = 1, ST(1) = x^4, ST(2) = x
+fld1                ; ST(0) = 1, ST(1) = x^4, ST(2) = x
 fsub ST(1), ST(0)   ; ST(0) = 1, ST(1) = x^4 - 1, ST(2) = x
 
 fxch ST(1)          ; ST(0) = x^4 - 1, ST(1) = 1, ST(2) = x
@@ -44,7 +49,7 @@ fsin                ; ST(0) = sin x^2, ST(1) = x, ST(2) = 2 * log2(sqrt(x^4 - 1)
 
 fsqrt               ; ST(0) = sqrt(sin x^2), ST(1) = x, ST(2) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6))
 
-fld one             ; ST(0) = 1, ST(1) = sqrt(sin x^2), ST(2) = x, ST(3) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6))
+fld1                ; ST(0) = 1, ST(1) = sqrt(sin x^2), ST(2) = x, ST(3) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6))
 fxch ST(1)          ; ST(0) = sqrt(sin x^2), ST(1) = 1, ST(2) = x, ST(3) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6))
 fyl2x               ; ST(0) = log2(sqrt(sin x^2)), ST(1) = x, ST(2) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6))
 
@@ -59,4 +64,54 @@ fsub ST(0), ST(1)   ; ST(0) = 2 * log2(sqrt(x^4 - 1)) / (pi / (sin x + pi / 6)) 
 
 ret
 calc ENDP
+
+compare proc C x:qword
+finit
+fld lowerBound      ; ST(0) = 1.1
+fld x               ; ST(0) = x, ST(1) = 1.1
+fcom
+fstsw ax
+sahf
+jb CASE_1
+
+fld upperBound      ; ST(0) = 5.1, ST(1) = x
+fcom
+fstsw ax
+sahf
+jae CASE_2
+
+fld nine            ; ST(0) = 9, ST(1) = 5.1, ST(2) = x
+fmul ST(0), ST(2)   ; ST(0) = 9x, ST(1) = 5.1, ST(2) = x
+fmul ST(0), ST(2)   ; ST(0) = 9x^2
+fld nine            ; ST(0) = 9, ST(1) = 9x^2
+fadd ST(0), ST(1)   ; ST(0) = 9x^2 + 9
+jmp CASE_END
+
+CASE_2:
+fxch ST(1)          ; ST(0) = x, ST(1) = 5.1
+fnstcw fsttmp
+or [fsttmp], 0C00h
+fldcw fsttmp
+frndint             ; ST(0) = цел(x)
+
+fld x               ; ST(0) = x, ST(1) = цел(x)
+fsub ST(0), ST(1)   ; ST(0) = ост(x), ST(1) = цел(x)
+f2xm1               ; ST(0) = 2^ост(x) - 1, ST(1) = цел(x)
+fxch ST(1)          ; ST(0) = цел(x), ST(1) = 2^ост(x) - 1
+fld1                ; ST(0) = 1, ST(1) = цел(x), ST(2) = 2^ост(x) - 1
+fadd ST(2), ST(0)   ; ST(0) = 1, ST(1) = цел(x), ST(2) = 2^ост(x)
+fxch ST(2)          ; ST(0) = 2^ост(x), ST(1) = цел(x), ST(2) = 1
+
+fscale              ; ST(0) = 2^x, ST(1) = цел(x), ST(2) = 1
+fsub ST(0), ST(2)   ; ST(0) = 2^x - 1
+
+jmp CASE_END
+
+CASE_1:
+fsincos             ; ST(0) = cos x, ST(1) = sin x
+fadd ST(0), ST(1)   ; ST(0) = sin x + cos x
+
+CASE_END:
+ret
+compare ENDP
 END
--- 2 курс/1 семестр/МЗЯП/ЛР 9/9/9/main.cpp
+++ 2 курс/1 семестр/МЗЯП/ЛР 9/9/9/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 extern "C" double calc(double);
+extern "C" double compare(double);
 
 int main() {
 	double a, x;
@@ -10,11 +11,37 @@ int main() {
 	cout << "x = ";
 	cin >> x;
 
-	a = (2 * log2(sqrt(pow(x, 4) - 1))) / (M_PI / (sin(x) + M_PI / 6)) - pow(tan(log2(sqrt(sin(x*x)))), 2);
-	cout << "(C++) a = " << a << endl;
+	cout << endl << "Task 1" << endl;
+	if (x > 1 && sin(x) != -M_PI / 6 && sin(x * x) > 0 && cos(log2(sqrt(sin(x * x)))) != 0)
+	{
+		a = (2 * log2(sqrt(pow(x, 4) - 1))) / (M_PI / (sin(x) + M_PI / 6)) - pow(tan(log2(sqrt(sin(x * x)))), 2);
+		cout << "(C++) a = " << a << endl;
 
-	a = calc(x);
-	cout << "(Asm) a = " << a << endl;
+		a = calc(x);
+		cout << "(Asm) a = " << a << endl;
+	}
+	else
+	{
+		cout << "Invalid x" << endl;
+	}
+
+	cout << endl << "Task 2" << endl;
+	if (x < 1.1)
+	{
+		a = sin(x) + cos(x);
+	}
+	else if (x <= 5.1)
+	{
+		a = pow(2, x) - 1;
+	}
+	else
+	{
+		a = 9*x*x + 9;
+	}
+	cout << "(C++) f(x) = " << a << endl;
+
+	a = compare(x);
+	cout << "(Asm) f(x) = " << a << endl;
 
 	return 0;
 }
