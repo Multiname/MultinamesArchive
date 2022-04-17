397c12a7ea035cf3a94c9ecbb67f593e1f7246f4
Multiname <minefend@gmail.com>
2022-04-17 12:46:52 +0300

    ВА ЛР 4 - первая часть: код и отчет

---
+++ 2 курс/2 семестр/Вычислительные алгоритмы/ЛР 4/VA_4_1.m
@@ -0,0 +1,74 @@
+clc;
+clear;
+clear global;
+
+figure
+fplot(@(x)sin(x),[-0.6 0.2]);
+hold on;
+grid on;
+
+x = [-0.6 -0.5 -0.3 -0.2 0 0.2];
+y = sin(x);
+
+figure
+plot(x, y,'-rs')
+grid on;
+
+global Matrix;
+
+xx = linspace(-0.6, 0.2, 81);
+Pix = zeros(1, length(x));
+dFx = zeros(1, length(x));
+Mi = zeros(1, length(x));
+Ri = zeros(1, length(x));
+
+for i = 1 : length(x)
+    t = zeros(1, i);
+    for j = 1 : i
+        t(j) = x(j);
+    end
+    
+    yy = newton(t, y, xx);
+    Pix(i) = yy(21);
+    dFx(i) = sin(-0.4) - yy(21);
+    
+    syms q;
+    Mi(i) = sin(fminbnd(inline(-diff(sin(q), i)), -0.6, x(i)));
+    
+    wi = 1;
+    for j = 1 : i
+        wi = wi * (-0.4 - x(j));
+    end
+    
+    Ri(i) = Mi(i) * wi / factorial(i);
+end
+
+yy = newton(x, y, xx);
+
+figure
+plot(x,y,'--rs',xx,yy, '-g')
+grid on;
+opengl('software');
+opengl('save','software');
+legend('Линейная интерполяция', 'Через интерполяционный многочлен Ньютона');
+
+names = {'i','Узлы','Pi(x*)','f(x*)-Pi(x*)','Mi+1','Ri(x*)'};
+i = [0;1;2;3;4;5];
+
+T = table(i,transpose(x),transpose(Pix),transpose(dFx),transpose(Mi),transpose(Ri),'VariableNames',names)
+
+function yy = newton(x, y, xx)
+    N = length(x);
+    global Matrix;
+    dif = y;
+    for  k = 1 : N-1
+        for i = 1: N - k
+            dif(i) = (dif(i+1) - dif(i)) / (x(i+k) - x(i))
+            Matrix(i, k) = dif(i);
+        end
+    end
+    yy = dif(1) * ones(size(xx));
+    for k = 2 : N
+        yy = dif(k) + (xx - x(k)) .* yy;
+    end
+end
